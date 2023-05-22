0d035d1547f26dfddd1edcc20d9c6b3bf7d665a9
Roshan Shaik <rshaik@ariqt.com>
2023-05-22 19:55:05 +0530

    check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+  getAgeInDogYears(): number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+
+  constructor(){}
+
+  person: Person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    getAgeInDogYears() {
+      return this.age * 7;
+    }
+  };
+
+  hello(){
+    this.person.firstName="Roshan";
+    console.log(this.person.firstName)
+  }
+
   title = 'translations';
 }
